222dd0d812ffc02295bbc53bb13ae121bf70ec56
Joseph Richardson <josephrichardson@Josephs-MacBook-Pro.local>
2014-04-22 21:16:11 -0700

    gathering correct mentors from each user. Need to work on display/edit user information

--- MentorWeb/MentorWebJS/JavaScriptMainMenu.js
+++ MentorWeb/MentorWebJS/JavaScriptMainMenu.js
@@ -1,122 +1,102 @@
 Parse.initialize("eBuYMHtEfY5emBFuHzQWHDjdxRsxxw3NQIQlrl9g", "cJeb591BLAskGN9bXsgPbY1c7uxdind30MfJUMPU");
 
+
+//all parse stuff will need to get rid of
+
+
 var moreParse = document.createElement('script');
 moreParse.type = "text/javascript";
 moreParse.src = "//www.parsecdn.com/js/parse-1.2.18.min.js";
 
+//jquery script need to keep
 var script = document.createElement('script');
 script.src = 'http://code.jquery.com/jquery-1.11.0.min.js';
 script.type = 'text/javascript';
 
-var resultsFromParse =new Array;
-(function gatherMentors()
+var resultsFromParse =new Array;//global array to hold all the users mentors 
+(function gatherMentors()//function to gather user mentors from database
 {
-    var mentor = Parse.Object.extend("Mentors");
-    var query = new Parse.Query(mentor);
-    query.find(
-    {
-        success: function (results)
+	var currUser = Parse.User.current();//parse call to get the current logged in user
+	var relation = currUser.relation("userMentors");//relation setup to gather all the user's mentors
+	
+	relation.query().find({//query call to find all the users mentors and store them into a list as parse object, will need to be mentor objects
+  	success: function(list) 
+  	{
+  		resultsFromParse = list;//copy the mentors list into the global array for use
+    	
+    	for (var i = 0; i < list.length; i++)//for loop logic to get the mentor info from each mentor object logic will need to be the same
         {
-			resultsFromParse = results;
-			
-            for (var i = 0; i < results.length; i++)
-            {
-                var object = results[i];
+        	    
+        		var object = list[i];
                 var firstName = object.get('firstName');
 				var lastName = object.get('lastName');
-				var url = object.get('image').url();
-                //need to display images here to mentee web network
-
-                
-                //alert(url);
-                //var mentorImage = document.createElement("BUTTON");
-                //mentorImage.style.backgroundImage
-                
-                //var mentorImage = document.createElement("img");
-                
-                 
-                //mentorImage.height = 60;
-                //mentorImage.width = 60;
-				
-				
-				
-                var label = "<br>First Name: " + firstName +"<br >Last Name: " + lastName + "<br /><br /><br /><br /><br /><br />";
-
-				//var url = object.get('image').url();
+				if(object.get('image') == null)
+				{
+					
+					var url = '/MentorWeb/MentorWeb/images/eclipse_update_120.jpg';
+				}
+				else
+				{
+					var url = object.get('image').url();
+				}
+				var label = "<br>First Name: " + firstName +"<br >Last Name: " + lastName + "<br /><br /><br /><br /><br />";
+				//create string to display content
 				var buttonName = i;
-				//alert(buttonName);
-				$('#myNetwork').append( "<button name = "+ buttonName+" class = 'getMentor' type='submit' onclick = 'displayInfo( this , event )'><img src= "+url+ " height='30' width = '30' /></button>");
-				alert("after append");
-				//$('#myNetwork').append( mentorImage);
-				//alert("hi ");
-                document.querySelector('#myNetwork').innerHTML += label; 
-                //displayInfo(1);
-
-            }
-   
-        },
-        error: function (error)
-        {
-            alert("Error: " + error.code + " " + error.message);
+				$('#myNetwork').append( "<button name = "+ buttonName+" class = 'getMentor' type='submit' onclick = 'displayInfo( this , event )'><img src= "+url+ " height='60' width = '60' /></button>");
+				//display content to iframe MenteeNetwork.html
+				
+                document.querySelector('#myNetwork').innerHTML += label;
         }
-    });
+  	}
+	});
+	
+	userProfileInfo(currUser);//function call to display user profile information 
+	
 })();
+function userProfileInfo(currUser)//function to display user's profile information in the user profile slide, logic needs to stay the same
+{
+	$('#userProfileInfo').empty();
+	
+	var firstName = currUser.get('firstName');
+	var lastName = currUser.get('lastName');
+	if(currUser.get('image') == null)
+	{
+					
+		var url = '/MentorWeb/MentorWeb/images/eclipse_update_120.jpg';
+	}
+	else
+	{
+		var url = currUser.get('image').url();
+	}
+	var label = "<br>First Name: " + firstName +"<br >Last Name: " + lastName + "<br />";
 
-function displayInfo( eventType, event)
+	document.querySelector('#userProfileInfo').append(label );
+	document.querySelector('#userProfileInfo').innerHTML = label ;
+}
+function displayInfo( eventType, event)//when user selects what mentor they want to look at then need to get mentor from global array and gather information to display
 {
 	$('#displayMentorInfo').empty();
-	//alert("eventType: " +eventType.name + "\n\nevet: " + event);
+	
 	var indexOfMentor = parseInt(eventType.name);
 	var selectedMentor = resultsFromParse[indexOfMentor];
 	var firstName = selectedMentor.get('firstName');
 	var lastName = selectedMentor.get('lastName');
 	var background = selectedMentor.get('background');
-	var url = selectedMentor.get('image').url();
+	
+	if(selectedMentor.get('image') != null)
+	{
+		var url = selectedMentor.get('image').url();
+	}
+	else
+	{
+		var url = '/MentorWeb/MentorWeb/images/eclipse_update_120.jpg';
+	}
+	
 	var description = "<br>" + firstName + "<br>" + lastName + "<br><br>Background: " + background;
-	//alert("Mentor Name" + selectedMentor.get('firstName'));
-	$('#displayMentorInfo').append( "<img src= "+url+ " height='100' width = '100' />");
+	
+	$('#displayMentorInfo').append( "<img src= "+url+ " height='135' width = '150' />");
+	
 	document.querySelector('#displayMentorInfo').innerHTML += description;
-	
-
-	
+		
 }
-// function display (array) {
-    // $.each(array, function(i, val){ 
-        // var galleryTitle=val[3],
-            // gallerySize=val[2],
-            // galleryDate=val[1],
-            // galleryDescription=val[4];
-// 
-        // $('#container').append(
-            // '<div id="'+array[i]+'" + <a href="#"><img src=images/'+array[i][5]+'></a></div>'
-        // );
-// 
-        // $("#"+i).hover( 
-            // function () { 
-                // $(this).append('<div id="description"> +'</div>');
-            // },      
-            // function () { 
-                // $(this).find("div:last").remove(); 
-            // } 
-        // );
-    // });
-// }
-	// $("#myNetwork").hover(function()
-	// {
-  		// //aler("hovering over mentor:" )
-  	// },function()
-  	// {
-  		// //alert("NEW MENTOR");
-	// });
-//from stack overflow trying to get button childnode index within div or name of button to get which index for which mentor to display
-	   // $(document).ready(function() {
-    // $(".getMentor").click(function(event) {
-        // alert(event.target.id);
-    // });
-
-//https://discussions.apple.com/thread/5474320?start=180&tstart=0
-//https://discussions.apple.com/message/22352505#22352505
-//http://support.apple.com/kb/DL1721
-//https://discussions.apple.com/message/23548999#23548999
-
 
